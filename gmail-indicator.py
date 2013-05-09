e1c883d4929f13d8c8fb3f1c988d107f7f6c5d39
Philipp Saveliev <fsfeel@gmail.com>
2013-05-10 01:37:24 +0400

    don't stop timer on error during fetching; minor improvements

--- gmail-indicator.py
+++ gmail-indicator.py
@@ -3,9 +3,14 @@ import contextlib
 import re
 import base64
 import argparse
+import traceback
+import logging
 import appindicator
 import gtk
 
+
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s %(funcName)s:%(lineno)d %(levelname)s: %(message)s')
+log = logging.getLogger(__name__)
 
 #https://developers.google.com/google-apps/gmail/gmail_inbox_feed
 GMAIL_FEED_URL = "https://mail.google.com/mail/feed/atom/"
@@ -16,13 +21,15 @@ def fetch_unread_num (user, password):
 	req.add_header('Authorization', "Basic " + base64.encodestring("%s:%s" % (user, password)))
 	with contextlib.closing(urllib2.urlopen(req)) as resp:
 		data = resp.read()
-	return re.search('<fullcount>(\d+)</fullcount>', data).group(1)
+	return int(re.search('<fullcount>(\d+)</fullcount>', data).group(1))
 
 def parse_args ():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-i', '--interval', type = int, default = 7, help = "Interval of checks in seconds")
 	parser.add_argument('credentials', nargs = '+', help = "E.g. name1 passwd1 name2 passwd2")
-	return parser.parse_args()
+	args = parser.parse_args()
+	assert len(args.credentials) % 2 == 0
+	return args
 
 def run ():
 	args = parse_args()
@@ -32,10 +39,9 @@ def run ():
 	ind = appindicator.Indicator("gmail-indicator-indicator", 'indicator-messages', appindicator.CATEGORY_APPLICATION_STATUS)
 	# ind.set_attention_icon('new-messages-red')
 	ind.set_status(appindicator.STATUS_ACTIVE)
-	ind.set_label("...")
+	ind.set_label("loading...")
 
 	menu = gtk.Menu()
-	# quit_item = gtk.MenuItem("Quit")
 	quit_item = gtk.ImageMenuItem(stock_id = gtk.STOCK_QUIT)
 	# menu.add(gtk.SeparatorMenuItem())
 	quit_item.connect('activate', lambda _widget: gtk.main_quit())
@@ -44,10 +50,19 @@ def run ():
 	ind.set_menu(menu)
 
 	def check_mail (once):
-		label = []
+		nums = []
 		for user, pwd in zip(*[iter(args.credentials)] * 2):
-			label.append(fetch_unread_num(user, pwd))
-		ind.set_label(", ".join(label))
+			try:
+				num = fetch_unread_num(user, pwd)
+			except:
+				log.error("Exception during fetching:\n%s" % traceback.format_exc())
+				if not ind.get_label().endswith('?'):
+					ind.set_label(ind.get_label() + "?")
+				return not once
+			else:
+				nums.append(str(num))
+
+		ind.set_label(", ".join(nums))
 		
 		return not once
 
